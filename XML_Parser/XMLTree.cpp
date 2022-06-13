4ea6b9089d95962e5ed044dbed18dc682f36374d
RadoslavBotov <92144419+RadoslavBotov@users.noreply.github.com>
2022-06-13 21:56:37 +0300

    shoud now support file paths correctly

--- XML_Parser/XMLTree.cpp
+++ XML_Parser/XMLTree.cpp
@@ -33,39 +33,88 @@ void XMLTree::runProgram()
 {
 	bool runProgram = true;		// tells us if we have to run dialog or not
 	bool fileOpen = false;		// we simulate a file; save, save-as, close, open all have an ofstream internally
-	std::string fileName = "testFile.xml";		// name of the file when user uses open
+	bool changesMade = false;	// tells us if user has made changes to the XML_Tree
+	std::string fileName;		// name of the file when user uses open
 	std::string newFileName;	// name of the file when user uses saveas as to not lose the original name of file
 	char userInput[128];		// the input we get from user
+	short indexOfCommand = -1;
 
 	root->key = "people";
 	root->addNode("person");
 	root->addNode("person");
 	root->addNode("person");
 	root->children[0]->addElement("name", "Ivan Burochovski");
-	root->children[1]->addElement("name", "Petur Georgiev");
+	root->children[0]->addElement("address", "Russia");
+	root->children[1]->addElement("name", "Alex Johns");
+	root->children[1]->addElement("address", "USA");
 	root->children[2]->addElement("name", "Radoslav Botov");
+	root->children[2]->addElement("address", "Bulgaria");
 
-	printHelp();				// prints instructions to console in the beginning of dialog
+	printHelp();		// prints instructions to console in the beginning of dialog
 
-	while (runProgram)			// while runProgramm is true, run dialog with user / until command exit
+	while (runProgram)	// while runProgramm is true, run dialog with user / until command exit
 	{
 		std::cout << "& ";							// prompt for user input
 		std::cin.getline(userInput, 128, '\n');		// get user input
 		
-		// open, close, save, saveas, help, exit <=> command
-		//    4,     5,    4,      6,    4,    4 <=> length of command
+		indexOfCommand = getIndexOfCommand(userInput);
+
 		if (strlen(userInput) > 6) // if length of command bigger than 6, there may be a name of file after command
 		{
-			// ... code
+			char tempBuffer[128]; // store command and a buffer for the file name as strncpy_s doesn't work with string
+
+			strncpy_s(tempBuffer, userInput, 5);  // we get first five symbols of user input
+			if (strcmp(tempBuffer, "open ") == 0) // we only need to see if they are the 'open' command
+			{
+				indexOfCommand = 0;
+				strncpy_s(tempBuffer, userInput + 5, strlen(userInput) - 4); // get the file name after open
+				fileName = tempBuffer;
+
+				// as we no longer need tempBuffer we can change it
+				if (strlen(tempBuffer) > 4)	// check if tempBuffer is bigger than 4 symbols so we can safely use strncpy below
+				{
+					strncpy_s(tempBuffer, tempBuffer + strlen(tempBuffer) - 4, 4);
+					if (strcmp(tempBuffer, ".xml") != 0) // check if fileName has '.xml' at the end
+						fileName += ".xml";				 // if not add it at the end
+				}
+				else						// if tempBuffer has less than 4 symbols we can safely say it doesn't
+					fileName += ".xml";		// contain '.xml' and we add it at the end
+			}
+			else
+			{
+				strncpy_s(tempBuffer, userInput, 7);    // we get first seven symbols of user input
+				if (strcmp(tempBuffer, "saveas ") == 0) // we only need to see if they are the 'saveas' command
+				{
+					indexOfCommand = 3;
+					strncpy_s(tempBuffer, userInput + 7, strlen(userInput) - 6); // get the file name after saveas
+				}
+				newFileName = tempBuffer;
+
+				// as we no longer need tempBuffer we can change it
+				if (strlen(tempBuffer) > 4)	// check if tempBuffer is bigger than 4 symbols so we can safely use strncpy below
+				{
+					strncpy_s(tempBuffer, tempBuffer + strlen(tempBuffer) - 4, 4);
+					if (strcmp(tempBuffer, ".xml") != 0) // check if fileName has '.xml' at the end
+						newFileName += ".xml";				 // if not add it at the end
+				}
+				else						// if tempBuffer has less than 4 symbols we can safely say it doesn't
+					newFileName += ".xml";		// contain '.xml' and we add it at the end
+			}
 		}
 
-		switch (getIndexOfCommand(userInput))	// get the command index the user inputs
+		
+		switch (indexOfCommand)	// get the command index the user inputs
 		{
 		case 0:	// open <file> is index 0
-			fileOpen = open(fileName);	// if file is successfully opened -> fileOpen will be true, otherwise it will be false
-			if(fileOpen)				// as such we can use it here instead of creating another boolean or calling open again
-				std::cout << "Successfully opened " + fileName << std::endl << std::endl;
-			// ... function that reads from file and save information as a tree with root the field 'root'
+			if (!fileOpen)
+			{
+				fileOpen = open(fileName);	// if file is successfully opened -> fileOpen will be true, otherwise it will be false
+				if (fileOpen)				// as such we can use it here instead of creating another boolean or calling open again
+					std::cout << "Successfully opened " + fileName << std::endl << std::endl;
+				// ... function that reads from file and save information as a tree with root the field 'root'
+			}
+			else
+				std::cout << "> Another file is already opened." << std::endl;
 			break;
 
 		case 1:	// close is index 1
@@ -104,19 +153,17 @@ void XMLTree::runProgram()
 			break;
 
 		case 5:	// exit is index 5
-			if (fileOpen) // check if we have an open file
+			if (fileOpen && changesMade) // check if we have an open file
 			{
-				std::cout << "> You have an open file with unsaved changes, please select 'close' or 'save' first." << std::endl << std::endl;
-				while (true)
+				std::cout << "> You have an open file with unsaved changes, please select 'close' or 'save' first." << std::endl;
+				while (true) // until user selects close or save
 				{
 					std::cout << "& ";	// user inputs save or saveas
 					std::cin.getline(userInput, 128, '\n');
 
 					short tempIndex = 1; // preset to close program without saving file changes
-					if (strcmp(userInput, "close") == 0)
-					{
+					if (strcmp(userInput, "close") == 0) // close has index 1
 						break;
-					}
 					else if (strcmp(userInput, "save") == 0)
 					{
 						tempIndex = 2; // set index to save file changes
@@ -138,7 +185,7 @@ void XMLTree::runProgram()
 
 short XMLTree::getIndexOfCommand(const char command[7])
 {
-	for (size_t i = 0; i < 6; i++)
+	for (short i = 0; i < 6; i++)
 		if (strcmp(command, commandLines[i]) == 0)
 			return i;
 	
@@ -163,8 +210,8 @@ bool XMLTree::save(std::string fileNameParam) const
 
 	if (!out)
 	{
-		std::cout << "> Error saving file to " + fileNameParam + ".xml" << std::endl;
-		std::cout << "> File not saved successfully." << std::endl;
+		std::cout << "> Error saving to " + fileNameParam << std::endl;
+		std::cout << "> File not saved successfully" << std::endl;
 		return false;	// return true as to NOT write in runProgram that the file was saved successfully
 	}
 
@@ -178,15 +225,24 @@ bool XMLTree::open(std::string fileNameParam)
 {
 	std::ifstream in(fileNameParam);
 
-	if (!in)	// if there was some error when opening file
+	if (!in)	// if there was some error when opening file, basically if we try to open a non-existing file
 	{
-		std::cout << "> Error reading from file " + fileNameParam + ".xml" << std::endl;	// inform user
-		std::cout << "> File not read successfully." << std::endl;
-		return false;		// return false as to NOT write in runProgram that the file was opened successfully
+		if (fileNameParam.find('\\') == -1) // if statement is true, fileNameParam doesn't contain '\\' 
+		{
+			std::ofstream out(fileNameParam);
+			out.close();
+			std::cout << "> " + fileNameParam + " does not exist." << std::endl;	// inform user
+			std::cout << "> An empty file with the same name was created." << std::endl << std::endl;
+			return false; // return false as to NOT write in runProgram that the file was opened successfully
+		}
+
+		std::cout << "> Error opening" + fileNameParam << std::endl;
+		std::cout << "> File not read successfully" << std::endl;
+		return false; // return false as to NOT write in runProgram that the file was opened successfully
 	}
 
-	//in >> root;		// read file and save as a tree in the root field
+	//in >> root;	  // read file and save as a tree in the root field
 	in.close();
 
-	return true;	// return true as to write in runProgram that the file was opened successfully
+	return true;      // return true as to write in runProgram that the file was opened successfully
 }
